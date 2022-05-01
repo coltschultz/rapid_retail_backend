a8ed59d833e25db703f212b2255667b217228e71
Colt <98503459+coltschultz@users.noreply.github.com>
2022-04-30 22:46:42 -0500

    seeds

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,12 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll()
+  .then(results => res.json(results))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
