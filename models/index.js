0280f08aa982accbb1d7b9d153924ee99c692661
Colt <98503459+coltschultz@users.noreply.github.com>
2022-04-30 22:14:35 -0500

    define columns in models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsToMany(Product, {
+  foreignKey: 'product_id'
+});
 
 module.exports = {
   Product,
