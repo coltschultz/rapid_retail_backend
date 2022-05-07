b094417d61be83acc1ecda0f3b6ad23c2d354f16
Colt <98503459+coltschultz@users.noreply.github.com>
2022-05-07 09:36:49 -0500

    update models and routes

--- Develop/seeds/product-tag-seeds.js
+++ Develop/seeds/product-tag-seeds.js
@@ -2,51 +2,51 @@ const { ProductTag } = require('../models');
 
 const productTagData = [
   {
-    product_id: 1,
+    product_ID: 1,
     tag_id: 6,
   },
   {
-    product_id: 1,
+    product_ID: 1,
     tag_id: 7,
   },
   {
-    product_id: 1,
+    product_ID: 1,
     tag_id: 8,
   },
   {
-    product_id: 2,
+    product_ID: 2,
     tag_id: 6,
   },
   {
-    product_id: 3,
+    product_ID: 3,
     tag_id: 1,
   },
   {
-    product_id: 3,
+    product_ID: 3,
     tag_id: 3,
   },
   {
-    product_id: 3,
+    product_ID: 3,
     tag_id: 4,
   },
   {
-    product_id: 3,
+    product_ID: 3,
     tag_id: 5,
   },
   {
-    product_id: 4,
+    product_ID: 4,
     tag_id: 1,
   },
   {
-    product_id: 4,
+    product_ID: 4,
     tag_id: 2,
   },
   {
-    product_id: 4,
+    product_ID: 4,
     tag_id: 8,
   },
   {
-    product_id: 5,
+    product_ID: 5,
     tag_id: 3,
   },
 ];
